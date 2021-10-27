51c729b92d284ff10c3a2b9044cec654528f0ee7
brenolyES <brenoly.porto@upe.br>
2021-10-27 10:47:42 -0300

    exercicios pos aula 26/10 finalizados

--- Modulo II - Javascript/Aula3/ErcerciciosPosAula/exe2.js
+++ Modulo II - Javascript/Aula3/ErcerciciosPosAula/exe2.js
@@ -1,1 +1,16 @@
+var listaNumeros = [5, 2, 4, 1, 3]
 
+function ordenar(listaNumeros){
+    for(let i=0; i < listaNumeros.length; i++){
+        for(let j=0; j < (listaNumeros.length - i - 1); j++){
+            if(listaNumeros[j] > listaNumeros[j+1]){
+                let maior = listaNumeros[j];
+                listaNumeros[j] = listaNumeros[j+1]
+                listaNumeros[j+1] = maior;
+            }
+        }
+    }
+    return listaNumeros;
+}
+
+console.log(ordenar(listaNumeros));
