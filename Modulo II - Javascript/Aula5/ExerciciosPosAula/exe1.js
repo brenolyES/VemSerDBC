4bc3ba13fc9413502beb1385768870c990cbe630
brenolyES <brenoly.porto@upe.br>
2021-10-29 09:28:04 -0300

    continuando a batalha com o excercicio da aula 5

--- Modulo II - Javascript/Aula5/ExerciciosPosAula/exe1.js
+++ Modulo II - Javascript/Aula5/ExerciciosPosAula/exe1.js
@@ -2,7 +2,8 @@ var listaDeProdutos = [];
 var id = 0;
 
 do{
-     var escolha = Number.parseInt(prompt("Escolha um opção:\n \n 1 - Para cadastrar um produto\n 2 - Excluir Produto\n 3 - Encontrar Produto\n 4 - Lista de produtos cadastrados\n 5 - Sair"));
+    
+    var escolha = Number.parseInt(prompt("Escolha um opção:\n \n 1 - Para cadastrar um produto\n 2 - Excluir Produto\n 3 - Encontrar Produto\n 4 - Lista de produtos cadastrados\n 5 - Sair"));
 
     if((typeof escolha === "number" && escolha.toString() !== "NaN") 
     && (escolha === 1 || escolha === 2 || escolha === 3 || escolha === 4 || escolha === 5) ){
@@ -18,35 +19,40 @@ do{
                 } 
 
                 cadastrarProduto();
+                listaDeProdutos.forEach(elemento => console.log(elemento));
 
                 break;
 
             case 2:
-                console.log('opção 2 escolhida')
-                escolha = 5;
+                
+                var excluirProdutos = (lista) => {
+                    var codigoParaEncontrar = Number.parseInt(prompt("Digite o codigo do produto que você deseja excluir: "));
+                    var listaFiltrada = lista.filter(elemento => elemento.codigo !== codigoParaEncontrar);
+                    return listaFiltrada;
+                }
+
+                listaDeProdutos = excluirProdutos(listaDeProdutos);
+                listaDeProdutos.forEach(elemento => console.log(elemento));
+
                 break;
+
             case 3:
                 
                 var encontrarProduto = (lista) => {
-                    var codigoParaEncontrar = Number.parseInt(prompt("Digite o codigo do produto que você quer encontrar: "))
+                    var codigoParaEncontrar = Number.parseInt(prompt("Digite o codigo do produto que você deseja encontrar: "))
                     var produtoEncontrado = lista.find(elemento => elemento.codigo === codigoParaEncontrar);
                     return produtoEncontrado;
                 }
 
                 console.log(encontrarProduto(listaDeProdutos));
-                5
 
                 break;
 
             case 4:
-                console.log(listaDeProdutos);
-                escolha = 5;
+                
+                listaDeProdutos.forEach(elemento => console.log(elemento));
+
                 break;
         }
-
-
-
-
-
     } else {alert("opção invalida")};
 }while(escolha != 5);
